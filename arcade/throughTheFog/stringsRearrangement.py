aa42307b400f09f8968adf6d5d84d6aedeaf222c
mabergerx <maberger0811@gmail.com>
2022-08-16 17:06:55 +0200

    Finished stringsRearrangement

--- arcade/throughTheFog/stringsRearrangement.py
+++ arcade/throughTheFog/stringsRearrangement.py
@@ -12,9 +12,14 @@ def solution(inputArray):
     
     for rearrangement in possible_rearrangements:
         distances_rearrangement = []
+        last_index = len(rearrangement) - 1
         for index, value in enumerate(rearrangement):
-            if index != len(rearrangement)-1:
+            if not all(distances_rearrangement):
+                break
+            if index != last_index:
                 distances_rearrangement.append(check_for_one(value, rearrangement[index+1]))
+
         if all(distances_rearrangement):
             return True
+            
     return False
