9c222fd0aff416fbddbf45bfc7280bc234b352b9
mabergerx <maberger0811@gmail.com>
2022-08-24 15:28:33 +0200

    Update growingPlant.py

--- arcade/darkWilderness/growingPlant.py
+++ arcade/darkWilderness/growingPlant.py
@@ -1,16 +1,14 @@
 def solution(upSpeed, downSpeed, desiredHeight):
-    height = 0
-    days = 0
-    while True:
-        days += 1
-        height = height + upSpeed
-        if height >= desiredHeight:
-            return days
-        height = height - downSpeed
-        if desiredHeight <= upSpeed:     
-            if height <= desiredHeight:
+    if desiredHeight <= upSpeed:     
+            return 1
+    else:
+        height = 0
+        days = 0
+        while True:
+            days += 1
+            height = height + upSpeed
+            if height >= desiredHeight:
                 return days
-        else:
+            height = height - downSpeed
             if height >= desiredHeight:
                 return days - 1
-                
