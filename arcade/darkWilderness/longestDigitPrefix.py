a8bd348f78ad34a0ec8d01aa814311f98d908fc9
mabergerx <maberger0811@gmail.com>
2022-09-02 10:49:53 +0200

    Update longestDigitPrefix.py

--- arcade/darkWilderness/longestDigitPrefix.py
+++ arcade/darkWilderness/longestDigitPrefix.py
@@ -1,16 +1,24 @@
+### Longer solution involved recursion through the list, but it only hit 10/12 tests strangely enough.
+
+# def solution(inputString):
+#     prefix = []
+#     inputString_list = list(inputString)
+    
+#     def recursive_pop(inputString_list, prefix):
+#         for index, el in enumerate(inputString_list):
+#             if el.isdigit():
+#                 prefix.append(el)
+#                 del inputString_list[index]
+#                 recursive_pop(inputString_list, prefix)
+#                 if len(prefix) == len(inputString):
+#                     return "".join(prefix)
+#             else:
+#                 return "".join(prefix)
+    
+#     return recursive_pop(inputString_list, prefix)
+
 def solution(inputString):
-    prefix = []
-    inputString_list = list(inputString)
-    
-    def recursive_pop(inputString_list, prefix):
-        for index, el in enumerate(inputString_list):
-            if el.isdigit():
-                prefix.append(el)
-                del inputString_list[index]
-                recursive_pop(inputString_list, prefix)
-                if len(prefix) == len(inputString):
-                    return "".join(prefix)
-            else:
-                return "".join(prefix)
-    
-    return recursive_pop(inputString_list, prefix)
+    for index, el in enumerate(inputString):
+        if not el.isdigit():
+            return inputString[:index]
+    return inputString
