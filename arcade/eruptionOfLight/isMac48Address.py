5dc7b80e8ec872c18d544afb0f815720d91c2eb9
mabergerx <maberger0811@gmail.com>
2022-09-03 17:06:51 +0200

    Create isMac48Address.py

---
+++ arcade/eruptionOfLight/isMac48Address.py
@@ -0,0 +1,9 @@
+def solution(inputString):
+    groups = inputString.split("-")
+    if len(groups) != 6:
+        return False
+    else:
+        for group in groups:
+            if not all([el in "0123456789ABCDEF" for el in group]):
+                return False
+    return True
